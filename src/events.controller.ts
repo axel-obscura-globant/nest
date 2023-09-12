eaf880401d21b0da84b4ee7823851ad9a6a7ee3d
Axel Laurent <axosar@gmail.com>
2023-09-12 09:50:03 -0600

    first commit

---
+++ src/events.controller.ts
@@ -0,0 +1,30 @@
+import { Controller, Get, Post, Patch, Delete, Param, Body, HttpCode} from '@nestjs/common';
+
+@Controller('/events')
+export class EventsController {
+  @Get()
+  findAll() {
+    return [
+      { id: 1, name: 'First Event' },
+      { id: 2, name: 'Second Event' },
+    ];
+  }
+  @Get(':id')
+  findOne(@Param('id') id) {
+    return { id: id, name: 'First Event' };
+  }
+
+  @Post()
+  create(@Body() input) {
+    return input;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id, @Body() input) {
+    return id;
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  remove(@Param('id') id) {}
+}
